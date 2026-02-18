da82ab42c77fd38e2e4606361cff3c2c351a857f
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-02-18 10:37:06 +0530

    second commit

--- assignment_2/even.java
+++ assignment_2/even.java
@@ -5,7 +5,7 @@ public class even{
 	public static void main(String []args){
 		Scanner sc = new Scanner(System.in);
 		int nth = sc.nextInt();
-		 for(int i=2;i<=nth;i=i+2){
+		 for(int i=0;i<=nth;i=i+2){
 			System.out.printf("even no. is : %d\n",i);
 		}
 		sc.close();
