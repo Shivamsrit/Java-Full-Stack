155c7dd2839d00d8a1e95a805a5e09c6e26e7beb
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-02-19 10:28:14 +0530

    third commit

--- assignment_2/reverse.java
+++ assignment_2/reverse.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class reversedigit{
+public class reverse{
 	public static void main(String []args){
 		Scanner sc = new Scanner(System.in);
 		int no=sc.nextInt();
---
+++ code_Snippt/DataTypes.java
@@ -0,0 +1,17 @@
+
+public class DataTypes{
+       public static void main(String []args){
+	/*int a=10;
+	int b=20;
+	byte c=(a>b)?30:40;
+	System.out.println(c);
+// It gives a compile-time error because, by default, the ternary operator returns an int type. It checks the condition and then stores the value. The compiler says there is a possible loss of data (int → byte).
+*/
+	int a=5   0;
+	int b=    50;
+	int z=50    ;
+	float f=123.76 86f;
+	double d=1 2 3 4;
+	
+	}	
+}
