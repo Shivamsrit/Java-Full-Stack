182679ad2042f81969d086654960c248a964525b
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-06-02 14:30:00 +0530

    Method example intro

---
+++ CH01CoreJava/CH15GenericClasses/ExMethodReference.java
@@ -0,0 +1,7 @@
+package CH15GenericClasses;
+
+/* to demonstrate the usage of for Each() to print the element of ArrayList
+=============================================================================
+ */
+import java.util.ArrayList;
+import java.util.function.Consumer;
