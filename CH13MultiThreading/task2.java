bca3948b330710b39c2dd4c57acd6c264c03f03b
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-06-03 12:36:04 +0530

    exception handling

---
+++ CH13MultiThreading/task2.java
@@ -0,0 +1,25 @@
+package CH13MultiThreading;
+
+class MyThread implements Runnable {
+    @Override
+    public void run() {
+        Thread.currentThread().setName("Rahul");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+        System.out.println("Mythread end");
+    }
+}
+
+public class task2 {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        Thread t1 = new Thread(myThread);
+        t1.start();
+        Thread.currentThread().setName("Shivam");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+        System.out.println("main thread end");
+    }
+}
