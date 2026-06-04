c4f36d8684645b2307c3e281b4b0bd53498bfa37
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-06-04 14:30:00 +0530

    Consumer Interface

---
+++ CH01CoreJava/CH16StreamAPI/CH01StreamAPI.java
@@ -0,0 +1,13 @@
+package CH16StreamAPI;
+
+/*
+        Stream API
+        ==========
+        * Stream => Channel through which there is a free flow movement of Data.
+        * Streams => To process Objects of the collection , In 1.8v Streams concept was Introduced.
+
+
+ */
+
+public class CH01StreamAPI {
+}
