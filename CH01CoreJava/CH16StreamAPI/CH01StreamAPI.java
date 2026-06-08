953e2bcf6d9d2dd6a23f8990932278f5b14556c8
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-06-08 14:30:00 +0530

    java io and util streams differ

--- CH01CoreJava/CH16StreamAPI/CH01StreamAPI.java
+++ CH01CoreJava/CH16StreamAPI/CH01StreamAPI.java
@@ -11,3 +11,12 @@ package CH16StreamAPI;
 
 public class CH01StreamAPI {
 }
+
+
+/*
+   * Difference between java.util.Streams and java.io.Streams
+   => java.util.Streams meant for processing objects from the Collection. That is it represents a
+   *  Stream of Objects From the Collection. But Java.io.Streams meant for Processing binary and
+   * Character data with respect to file that is it represents Stream of Binary data or Character
+   * data from file , hence both java.io.Streams and Java.util.Streams both are different.
+
