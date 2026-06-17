e4da4fed9e8e0a4617a5ad7995ab3a861fc15577
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-06-17 14:30:00 +0530

    FileHandling Intro

---
+++ CH01CoreJava/CH17FileHandling/FileHandling.java
@@ -0,0 +1,24 @@
+ackage CH17FileHandling;
+
+/*
+ * Understanding File Handling in Java
+ */
+
+/*
+ * File handling in Java allows you to work with files and directories.
+ * Java provides several classes for file operations:
+ *
+ * Main Classes:
+ * - File: Represents file and directory pathnames
+ * - FileInputStream/FileOutputStream: For reading/writing bytes
+ * - FileReader/FileWriter: For reading/writing characters
+ * - BufferedReader/BufferedWriter: For efficient reading/writing
+ * - Scanner: For reading formatted input
+ * - PrintWriter: For formatted output
+ * - Files (java.nio.file): Modern approach with utility methods
+ *
+ * Common Operations:
+ * - Creating, reading, writing, deleting files
+ * - Checking file properties (exists, readable, writable)
+ * - Working with directories
+ */
