3207dff00668337871f70ba5f56feaff3ee8f67f
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-06-26 15:11:47 +0530

    Generics method

--- CH01CoreJava/CH15GenericClasses/CH01GenricsPart2.java
+++ CH01CoreJava/CH15GenericClasses/CH01GenricsPart2.java
@@ -3,6 +3,8 @@ package CH01CoreJava.CH15GenericClasses;
 
 // 2. custom created Generic class
 
+import java.util.ArrayList;
+
 class Gen <T>{
     T obj;
 
@@ -41,6 +43,26 @@ class Test1 <T extends Runnable> {      // Runnable --> Interface
 
 }
 
+// * class demo<X,Y>{                //it allows
+
+//    }
+
+
+/*
+            class test<t extends Number&Runnable> // valid
+            class test<t extends Number&Runnable&Comparable> // valid
+            class test<t extends Number&String> // Invalid because we can,t extend more than one class at a time
+            class test<t extends Comparable&Runnable> // Invalid
+            class test<t extends Runnable&Number> // Invalid
+
+Generic class
+=============
+        Normal class = Type Parameter
+
+        can you apply type parameter at method level
+        Ans:- yes, it is possible
+
+ */
 public class CH01GenricsPart2 {
     public static void main(String[] args) {
         Gen t = new Gen<>("pankaj");
@@ -52,5 +74,38 @@ public class CH01GenricsPart2 {
         obj.show();
         System.out.println("! 2st Generic class"+obj.getObj());
 
+
+        ArrayList<Integer> li = new ArrayList<>();
+        li.add(20);
+        li.add(40);
+        li.add(89);
+        ;;
+        ;;
+
+        CH01GenricsPart2 ch = new CH01GenricsPart2();
+        ch.m1(li);
     }
+
+//    Generic Method And wild card Character pattern(?)
+//  =================================================
+    public void m1(ArrayList< ? > li){
+
+    }
+
+//          There will be 4 test cases
+/*    ===================================
+
+            1. m1(Arraylist<String> li)
+            2. m1(Arraylist<?> li)
+                 * This method only applicable for ArrayList of any Type.
+                 * This method signature is best suited only we perform read operation on Arraylist.
+                 * This method we can,t add anything to the list expected null.
+            3. m1(Arraylist< ? extends X > li)   *
+            4. m1(Arraylist< ? Super x> li)
+                * X--> class , we can make a call to method by passing ArrayList of X Type and its super class.
+                * X--> interface , we can make a call to method by passing ArrayList of X type or its super class of implementation class of X.
+                
+
+
+ */
 }
