f60e960b8e9fdf8ce3b5093014bb720283733618
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-07-02 14:30:00 +0530

    Method Reference update

--- CH01CoreJava/CH15GenericClasses/CH05MethodReference.java
+++ CH01CoreJava/CH15GenericClasses/CH05MethodReference.java
@@ -19,3 +19,39 @@ package CH01CoreJava.CH15GenericClasses;
             void run();
    }
  */
+interface inter{
+    void print(int i);
+}
+
+public class CH05MethodReference {
+    public static void m1(){
+        for(int i =0 ; i< 10 ; i++){
+            System.out.println("child thread Created");
+        }
+    }
+    public  void Print(int i){
+        // Method Reference
+        System.out.println("Method binded by object reference");
+    }
+    public static void main(String[] args) throws InterruptedException{
+
+ //  Using Method Reference binded the method call run() of Interface Runnable.
+        Runnable r = CH05MethodReference::m1;
+
+        Thread t = new Thread(r);
+        t.start();
+
+        inter i1 = x -> System.out.println("By lambda expression");
+
+        inter i2 = new CH05MethodReference()::Print;
+
+        i1.print(10);
+        i2.print(10);
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("Main Thread Created");
+            Thread.sleep(2000);
+        }
+    }
+
+}
